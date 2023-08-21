a0039a24f1d89f8e91f0a9b7e3821dab27dfc3c8
Geitodyu Henrique Crusoe <crusoehenrique3@gmail.com>
2023-08-21 16:59:57 -0700

    betty style fixed

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * int _strlen - a function that returns the length of a string
- *
+ * _strlen - a function that returns the length of a string
+ * @s:- pointer string
  * Return: Always 0.
  */
 int _strlen(char *s)
 {
 	int length = 0;
 
-	while (*s != '\0') {
+	while (*s != '\0')
+	{
 		length++;
 		s++;
 	}
-	
-	return length;
+	return (length);
 }
