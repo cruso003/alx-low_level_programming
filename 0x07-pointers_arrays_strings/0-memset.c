44e149a7ded5672b67e385f34e94aa485cc9eb38
Henrique <crusoehenrique3@gmail.com>
2023-08-28 23:23:22 +0300

    first commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte
+ *
+ * @s: original pointer 
+ * @b: set original pointer to b
+ * @n: allocated memory size
+ *
+ * Return: pointer to the memory
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *original = s;
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*s = b;
+		s++;
+	}
+	return (original);
+}
