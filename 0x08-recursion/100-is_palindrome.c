2f0e3208604e7ab2ff56d45c67aba11860e6a44e
Geitodyu Henrique Crusoe <crusoehenrique3@gmail.com>
2023-09-01 01:33:20 +0300

    update

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,10 +1,10 @@
 /**
- *  * is_palindrome - checks if a string is a palindrome
- *   * @s: string to check
- *    *
- *     * Return: 1 if it's a palindrome, 0 if not
- *      */
-int check_palindrome(char *start, char *end);
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to check
+ *
+ * Return: 1 if it's a palindrome, 0 if not
+ */
+int is_palindrome_recursive(char *s, int start, int end);
 
 int is_palindrome(char *s) {
 	    int length = 0;
@@ -12,18 +12,17 @@ int is_palindrome(char *s) {
 			        length++;
 				    }
 		    
-		    return check_palindrome(s, s + length - 1);
+		    return is_palindrome_recursive(s, 0, length - 1);
 }
 
-int check_palindrome(char *start, char *end)
-{
+int is_palindrome_recursive(char *s, int start, int end) {
 	    if (start >= end) {
 		            return 1;
 			        }
 	        
-	        if (*start != *end) {
+	        if (s[start] != s[end]) {
 			        return 0;
 				    }
 		    
-		    return check_palindrome(start + 1, end - 1);
+		    return is_palindrome_recursive(s, start + 1, end - 1);
 }
