996552a48d55d3ec2ef8475943eff3dd2f3f8d1a
Geitodyu Henrique Crusoe <crusoehenrique3@gmail.com>
2023-09-06 21:21:49 +0300

    first task

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *tetama = malloc(sizeof(char) * size);
 
-	if (tetama == NULL)
+	if (tetama == NULL || size == 0)
 	{
 		return (NULL);
 	}
