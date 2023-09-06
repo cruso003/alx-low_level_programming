7d2b50a982666fd99926f75ecf79176360fe2cd3
Geitodyu Henrique Crusoe <crusoehenrique3@gmail.com>
2023-09-06 23:41:30 +0300

    task 3-alloc_grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,6 +25,11 @@ int **alloc_grid(int width, int height)
 		grid[i] = (int *)malloc(height * sizeof(int));
 		if (grid[i] == NULL)
 		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
 			return (NULL);
 		}
 		for (j = 0; j < width; j++)
