ccdee35b928e096eb79411eba460b0c88cdb050b
Geitodyu Henrique Crusoe <crusoehenrique3@gmail.com>
2023-09-06 21:33:04 +0300

    log fix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,7 +5,7 @@
  * with a specific char.
  * @size: input integer
  * @c: input character
- * Return: pointer to the array, or Null
+ * Return: pointer to the array, or Null if size = 0
  */
 char *create_array(unsigned int size, char c)
 {
