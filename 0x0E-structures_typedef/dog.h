da81095dbc42b5505cc7de25000c31ac6824c236
Geitodyu Henrique Crusoe <crusoehenrique3@gmail.com>
2023-09-12 00:00:28 +0300

    first task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - defines a structure.
+ * @name: first member
+ * @age: second member
+ * @owner: last member
+ *
+ * Description: This defines a structure called dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
