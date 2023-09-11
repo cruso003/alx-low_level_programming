1a082d86f7aa585aebe67321fe6626bac38a9643
Geitodyu Henrique Crusoe <crusoehenrique3@gmail.com>
2023-09-11 21:51:57 +0300

    task 3

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - prints the name of the file it was complied from
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
