070b1443267753185d09df5c8a552f34470926ec
Geitodyu Henrique Crusoe <crusoehenrique3@gmail.com>
2023-09-13 07:58:20 +0300

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: A pointer to a null-terminated string containing printed name
+ * @f: a function pointer pointing to a function capable of printing a char
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
