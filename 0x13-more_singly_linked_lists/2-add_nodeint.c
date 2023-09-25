229d6245c859e1b9919b220007c65d33e1bb31d6
Geitodyu Henrique Crusoe <crusoehenrique3@gmail.com>
2023-09-26 01:40:52 +0300

    First batch

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,20 +11,11 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *newNode;
 
-	if (!n)
-	{
-		return (NULL);
-	}
 	newNode = (listint_t *)malloc(sizeof(listint_t));
-	if (newNode == NULL)
-	{
+	if (!newNode)
 		return (NULL);
-	}
-	else
-	{
-		newNode->n = n;
-		newNode->next = *head;
-		*head = newNode;
-	}
+	newNode->n = n;
+	newNode->next = *head;
+	*head = newNode;
 	return (newNode);
 }
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -17,7 +17,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		return (NULL);
 	}
 	newNode = (listint_t *) malloc(sizeof(listint_t));
-	if (newNode == NULL)
+	if (!newNode)
 	{
 		return (NULL);
 	}
