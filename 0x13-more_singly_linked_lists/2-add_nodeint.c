fc35157214495c60a6a91b88ae9575aa3f55830d
Geitodyu Henrique Crusoe <crusoehenrique3@gmail.com>
2023-09-26 01:45:23 +0300

    First batch

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,11 +11,13 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *newNode;
 
-	newNode = (listint_t *)malloc(sizeof(listint_t));
+	newNode = malloc(sizeof(listint_t));
 	if (!newNode)
 		return (NULL);
+
 	newNode->n = n;
 	newNode->next = *head;
 	*head = newNode;
+
 	return (newNode);
 }
