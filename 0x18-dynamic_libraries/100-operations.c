e89c351ee12d15333274066a3d8474530b56633d
Geitodyu Henrique Crusoe <crusoehenrique3@gmail.com>
2023-11-17 00:14:25 +0300

    fix

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -2,29 +2,29 @@
 
 int add(int a, int b)
 {
-	return a + b;
+	return (a + b);
 }
 
 int sub(int a, int b)
 {
-	return a - b;
+	return (a - b);
 }
 
 int mul(int a, int b)
 {
-	return a * b;
+	return (a * b);
 }
 
 int div(int a, int b)
 {
 	if (b != 0)
 	{
-		return a / b;
+		return (a / b);
 	}
 	else
 	{
 		printf("Error: Division by zero\n");
-		return 0;
+		return (0);
 	}
 }
 
@@ -32,11 +32,11 @@ int mod(int a, int b)
 {
 	if (b != 0)
 	{
-		return a % b;
+		return (a % b);
 	}
 	else
 	{
 		printf("Error: Modulo by zero\n");
-		return 0;
+		return (0);
 	}
 }
